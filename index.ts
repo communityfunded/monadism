54988ef513c69b3ea4d04dc20f411206b95ce7b7
Brandon Konkle <brandon@konkle.us>
2019-04-05 20:42:11 -0600

    Adjust the index a bit

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import Maybe from './src/Maybe'
+import Either from './src/Either'
+
+export {Maybe, Either}
+
 export * from './src/Functional'
 export * from './src/Maybe'
 export * from './src/Either'
