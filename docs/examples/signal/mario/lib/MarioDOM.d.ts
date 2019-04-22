7bfbb081a8578f582f101de49527b8c0e97c507c
Brandon Konkle <brandon@ecliptic.io>
2019-04-22 16:40:27 -0600

    Rebuild docs

--- docs/examples/signal/mario/lib/MarioDOM.d.ts
+++
@@ -1,5 +0,0 @@
-import { Character } from './Mario';
-export declare const updatePosition: (c: Character) => Character;
-export declare const updateSprite: (c: Character) => Character;
-export declare const onDOMContentLoaded: (action: () => void) => void;
-export declare const getMarioNode: () => HTMLElement;
